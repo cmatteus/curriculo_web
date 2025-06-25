c26430e30904bd2d81545b7176e5f6c327c37a0e
Cauã Matteus Vieira Pereira <cauamatteus.ti@gmail.com>
2025-06-25 19:41:16 -0300

    Atualizações no código

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Projeto(models.Model):
+    titulo = models.CharField(
+        null=False,
+        blank=False,
+        max_length=100,
+        verbose_name="Título"
+    )
+    imagem = models.ImageField(
+        null=False,
+        blank=False,
+        upload_to='projetos/',
+        verbose_name="Imagem",
+        help_text="Faça o upload da imagem do projeto"
+    )
+    descricao = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name="Descrição"
+    )
+    link_online = models.URLField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name="Link para o projeto online"
+    )
+    link_github = models.URLField(
+        blank=True,
+        null=True,
+        max_length=200,
+        verbose_name="Link para o GitHub"
+    )
