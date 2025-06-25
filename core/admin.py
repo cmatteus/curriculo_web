434a5899f4d89e0eb33aa7b21406c3edc84b4fa7
Cauã Matteus Vieira Pereira <cauamatteus.ti@gmail.com>
2025-06-25 19:51:12 -0300

    Projeto ativo

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Projeto
+
+@admin.register(Projeto)
+class ProjetoAdmin(ModelAdmin):
+    list_display = ('titulo', 'link_github')
