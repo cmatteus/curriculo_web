d6d10ee6d718b7d66bf1eaa16a12e86029bc04a8
Cauã Matteus Vieira Pereira <cauamatteus.ti@gmail.com>
2025-06-25 20:13:15 -0300

    Att

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from .models import Projeto
+from .models import Projeto, Experiencia
 
 @admin.register(Projeto)
 class ProjetoAdmin(ModelAdmin):
     list_display = ('titulo', 'link_github')
+
+@admin.register(Experiencia)
+class ExperienciaAdmin(ModelAdmin):
+    list_display = ('cargo', 'nome_empresa', 'data_inicio', 'data_fim', 'emprego_atual')
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Projeto(models.Model):
     titulo = models.CharField(
@@ -31,3 +32,61 @@ class Projeto(models.Model):
         max_length=200,
         verbose_name="Link para o GitHub"
     )
+
+    def __str__(self):
+        return self.titulo
+
+class Experiencia(models.Model):
+    cargo = models.CharField(
+        blank=False,
+        null=False, 
+        max_length=200,
+        verbose_name="Cargo")
+    nome_empresa = models.CharField(
+        blank=False,
+        null=False, 
+        max_length=200,
+        verbose_name="Nome da Empresa")
+    icone_empresa = models.ImageField(
+        upload_to='icones_empresas/',
+        blank=False,
+        null=False, 
+        verbose_name="Ícone da Empresa",
+        help_text="Faça o upload do logo/ícone da empresa"
+    )
+    data_inicio = models.DateField(
+        blank=False,
+        null=False, 
+        verbose_name="Data de Início"
+    )
+    emprego_atual = models.BooleanField(
+        default=False,
+        verbose_name="Este é o emprego atual?",
+        help_text="Marque esta opção se você ainda trabalha nesta empresa."
+    )
+    data_fim = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Data de Fim",
+        help_text="Deixe em branco se for o emprego atual."
+    )
+    descricao = models.TextField(
+        verbose_name="Descrição das Atividades"
+    )
+
+    def clean(self):
+        """
+        Adiciona uma validação customizada para garantir a lógica entre os campos.
+        """
+        super().clean()
+        if self.emprego_atual and self.data_fim:
+            raise ValidationError(
+                "Se 'Emprego Atual' está marcado, o campo 'Data de Fim' não pode ser preenchido."
+            )
+        if not self.emprego_atual and not self.data_fim:
+             raise ValidationError(
+                "Se este não é o seu emprego atual, você precisa preencher o campo 'Data de Fim'."
+            )
+
+    def __str__(self):
+        return f"{self.cargo} na empresa {self.nome_empresa}"
