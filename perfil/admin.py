c258b0d835e7a98e9562645c359e6066cae4b0f9
Cauã Matteus Vieira Pereira <cauamatteus.ti@gmail.com>
2025-06-26 09:05:04 -0300

    Perfil Criado

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from .models import InformacaoPessoal
+from .models import InformacaoPessoal, RedesSociais
 
 @admin.register(InformacaoPessoal)
 class InformacaoPessoalAdmin(ModelAdmin):
     list_display = ('nome_completo',)
+
+@admin.register(RedesSociais)
+class RedeSocialAdmin(ModelAdmin):
+    list_display = ('linkedin', 'instagram', 'github')
