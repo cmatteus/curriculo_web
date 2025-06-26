19b4c9a9e06ce112438899574300c7495d42d512
Cauã Matteus Vieira Pereira <cauamatteus.ti@gmail.com>
2025-06-25 21:13:41 -0300

    Projeto atualização

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from .models import Projeto, Experiencia
+from .models import Projeto, Experiencia, Formacao, Lingua, Educacao, Habilidade
 
 @admin.register(Projeto)
 class ProjetoAdmin(ModelAdmin):
@@ -9,3 +9,21 @@ class ProjetoAdmin(ModelAdmin):
 @admin.register(Experiencia)
 class ExperienciaAdmin(ModelAdmin):
     list_display = ('cargo', 'nome_empresa', 'data_inicio', 'data_fim', 'emprego_atual')
+
+@admin.register(Lingua)
+class LinguaAdmin(ModelAdmin):
+    list_display = ('lingua', 'nivel')
+
+@admin.register(Habilidade)
+class HabilidadeAdmin(ModelAdmin):
+    list_display = ('habilidade', 'tipo')
+
+
+@admin.register(Educacao)
+class EducacaoAdmin(ModelAdmin):
+    list_display = ('titulo', 'instituicao', 'tipo', 'data_conclusao')
+
+
+@admin.register(Formacao)
+class FormacaoAdmin(ModelAdmin):
+    list_display = ('curso', 'instituicao', 'grau', 'data_inicio', 'data_fim', 'em_andamento')
--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class Projeto(models.Model):
     titulo = models.CharField(
@@ -32,6 +33,10 @@ class Projeto(models.Model):
         max_length=200,
         verbose_name="Link para o GitHub"
     )
+
+    class Meta:
+        verbose_name = "Projeto"
+        verbose_name_plural = "Projetos"
 
     def __str__(self):
         return self.titulo
@@ -87,6 +92,208 @@ class Experiencia(models.Model):
              raise ValidationError(
                 "Se este não é o seu emprego atual, você precisa preencher o campo 'Data de Fim'."
             )
+    
+    class Meta:
+        verbose_name = "Experiência Profissional"
+        verbose_name_plural = "Experiências Profissionais"
 
     def __str__(self):
         return f"{self.cargo} na empresa {self.nome_empresa}"
+    
+class Lingua(models.Model):
+
+    class Nivel(models.TextChoices):
+        ELEMENTAR = 'Elementar', 'Elementar'
+        BASICO = 'Básico', 'Básico'
+        BASICO_INTERMEDIARIO = 'Básico-Intermediário', 'Básico-Intermediário'
+        INTERMEDIARIO = 'Intermediário', 'Intermediário'
+        INTERMEDIARIO_AVANCADO = 'Intermediário-Avançado', 'Intermediário-Avançado'
+        AVANCADO = 'Avançado', 'Avançado'
+        FLUENTE = 'Fluente', 'Fluente'
+        NATIVO = 'Nativo', 'Nativo'
+
+    lingua = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100,
+        verbose_name="Língua"
+    )
+    icone = models.ImageField(
+        upload_to='icones_linguas/',
+        blank=False,
+        null=False,
+        verbose_name="Ícone da Língua"
+    )
+    nivel = models.CharField(
+        blank=False,
+        null=False,
+        choices=Nivel.choices,
+        verbose_name="Nível"
+    )
+
+    class Meta:
+        verbose_name = "Língua"
+        verbose_name_plural = "Línguas"
+
+    def __str__(self):
+        return f"{self.lingua} ({self.get_nivel_display()})"
+    
+class Habilidade(models.Model):
+
+    class Tipo(models.TextChoices):
+        HARD_SKILL = 'HARD', 'Hard Skill'
+        SOFT_SKILL = 'SOFT', 'Soft Skill'
+
+    habilidade = models.CharField(
+        blank=False,
+        null=False, 
+        max_length=100,
+        verbose_name="Habilidade"
+    )
+    icone = models.ImageField(
+        upload_to='icones_habilidades/',
+        blank=False,
+        null=False, 
+        verbose_name="Ícone da Habilidade"
+    )
+    tipo = models.CharField(
+        blank=False,
+        null=False, 
+        max_length=4,
+        choices=Tipo.choices,
+        default=Tipo.HARD_SKILL,
+        verbose_name="Tipo de Habilidade"
+    )
+    descricao = models.TextField(
+        blank=True,
+        null=True, 
+        verbose_name="Descrição",
+        help_text="Descreva brevemente a habilidade."
+    )
+
+    class Meta:
+        verbose_name = "Habilidade"
+        verbose_name_plural = "Habilidades"
+
+    def __str__(self):
+        return f"{self.habilidade} - {self.get_tipo_display()}"
+    
+class Educacao(models.Model):
+
+    class Tipo(models.TextChoices):
+        CURSO = 'CURSO', 'Curso'
+        CERTIFICACAO = 'CERT', 'Certificação'
+
+    titulo = models.CharField(
+        blank=False,
+        null=False,
+        max_length=200,
+        verbose_name="Título"
+    )
+    instituicao = models.CharField(
+        blank=False,
+        null=False,
+        max_length=200,
+        verbose_name="Instituição / Empresa"
+    )
+    tipo = models.CharField(
+        blank=False,
+        null=False,
+        max_length=5,
+        choices=Tipo.choices,
+        default=Tipo.CURSO,
+        verbose_name="Tipo"
+    )
+    data_conclusao = models.DateField(
+        blank=False,
+        null=False,
+        verbose_name="Data de Conclusão"
+        )
+    descricao = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Descrição"
+    )
+    link_certificado = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name="Link para o Certificado"
+    )
+
+    class Meta:
+        verbose_name = "Formação Educacional"
+        verbose_name_plural = "Formações Educacionais"
+
+    def __str__(self):
+        return self.titulo
+    
+class Formacao(models.Model):
+
+    class Grau(models.TextChoices):
+        ENSINO_FUNDAMENTAL = 'Ensino Fundamental', 'Ensino Fundamental'
+        ENSINO_MEDIO = 'Ensino Médio', 'Ensino Médio'
+        CURSO_TECNICO = 'Curso Técnico', 'Curso Técnico'
+        CURSO_SUPERIOR = 'Curso Superior', 'Curso Superior'
+        ESPECIALIZACAO = 'Especialização', 'Especialização'
+        MBA = 'MBA', 'MBA'
+        MESTRADO = 'Mestrado', 'Mestrado'
+        DOUTORADO = 'Doutorado', 'Doutorado'
+
+    curso = models.CharField(
+        blank=False,
+        null=False,
+        max_length=200,
+        verbose_name="Curso"
+    )
+    instituicao = models.CharField(
+        blank=False,
+        null=False,
+        max_length=200,
+        verbose_name="Instituição"
+    )
+    logo_instituicao = models.ImageField(
+        upload_to='logos_instituicoes/',
+        blank=False,
+        null=False,
+        verbose_name="Logo da Instituição"
+    )
+    grau = models.CharField(
+        blank=False,
+        null=False,
+        choices=Grau.choices,
+        verbose_name="Grau"
+    )
+    data_inicio = models.DateField(
+        blank=False,
+        null=False,
+        verbose_name="Data de Início"
+    )
+    em_andamento = models.BooleanField(
+        default=False,
+        verbose_name="Em andamento?",
+        help_text="Marque esta opção se esta formação ainda não foi concluída."
+    )
+    data_fim = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Data de Fim (ou Previsão)",
+        help_text="Deixe em branco ou use uma data futura se estiver em andamento."
+    )
+
+    def clean(self):
+        """
+        Validação atualizada:
+        - Se a formação NÃO está em andamento, a data de fim é obrigatória.
+        """
+        super().clean()
+        if not self.em_andamento and not self.data_fim:
+             raise ValidationError(
+                {'data_fim': "Se esta formação não está em andamento, você precisa preencher o campo 'Data de Fim'."}
+            )
+
+    class Meta:
+        verbose_name = "Formação Acadêmica"
+        verbose_name_plural = "Formações Acadêmicas"
+
+    def __str__(self):
+        return f"{self.grau} em {self.curso} - {self.instituicao}"
